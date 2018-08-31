1c0ebc27ff50cff66dad085dedfd43385108adcb
Tairan Liu <liutairan2012@gmail.com>
2018-08-31 13:13:59 -0500

    Add main file

---
+++ src/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import string
+from random import *
+from PyQt5.QtWidgets import (QApplication)
+from PyQt5.QtGui import QIcon
+
+from MainWindow import App
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Pics', 'icon.png')
+    app.setWindowIcon(QIcon(path))
+    mainWindow = App()
+    sys.exit(app.exec_())
