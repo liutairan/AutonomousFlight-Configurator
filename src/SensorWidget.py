d7d601115db753606a4701024d35a7b3ff42edc7
Tairan Liu <liutairan2012@gmail.com>
2019-08-11 23:24:10 -0500

    Update SensorWidget.py

--- src/SensorWidget.py
+++ src/SensorWidget.py
@@ -38,7 +38,7 @@ import subprocess
 import threading
 from functools import partial
 import numpy as np
-from apscheduler.schedulers.background import BackgroundScheduler
+# from apscheduler.schedulers.background import BackgroundScheduler
 from random import *
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QPushButton,
     QListWidget, QListWidgetItem, QAbstractItemView, QWidget, QAction,
